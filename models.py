00e7d0d613bb97f7234ef7577752331f8cba79c5
Eden Zhang <53964412+Velocity9013@users.noreply.github.com>
2020-08-07 22:20:46 +1200

    Initial Commit

---
+++ models.py
@@ -0,0 +1,27 @@
+import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+image_size = 28
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.Sequential() 
+model.add(tf.keras.layers.Flatten(input_shape=(image_size, image_size))) 
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy', 
+              metrics=['accuracy'] 
+              ) 
+
+model.fit(x_train,y_train,epochs=10) 
+model.save('number_identifier.h5') 
+new_model = tf.keras.models.load_model('number_identifier.h5') 
